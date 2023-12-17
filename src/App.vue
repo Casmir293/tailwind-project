5989a04cedc364c8174ea57844a787218750d73c
Casmir Onyeka <casmir293@gmail.com>
2023-12-17 10:39:15 +0100

    tailwind project setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <div>
+      <HelloWorld />
+    </div>
+  </header>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-3xl font-bold underline bg-yellow-400">Hello world!</h1>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
