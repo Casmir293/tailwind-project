bb77703e45a8b6c5a9c473074aa14c3efbdfa658
Casmir Onyeka <casmir293@gmail.com>
2023-12-20 10:58:40 +0100

    added logo and nav icon

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,9 @@
 <template>
-  <header class="w-100 h-10 bg-green-600">
-    <section class="mx-5 bg-green-600"></section>
+  <header class="w-100 fixed left-0 right-0 top-0 h-14 bg-green-800">
+    <section class="flex h-14 items-center justify-between px-3">
+      <section class=""><h1>🚀 Acme Rockets</h1></section>
+      <section>&#9776;</section>
+    </section>
   </header>
 </template>
 
