569c963f95b1dcd7f87e1b18655cf3caf3629307
Casmir Onyeka <casmir293@gmail.com>
2023-12-20 12:16:20 +0100

    finished desktop nav UI

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -4,7 +4,23 @@
       class="m-auto flex h-14 max-w-5xl items-center justify-between px-3 text-xl font-bold"
     >
       <a href="#">🚀 Acme Rockets</a>
-      <section class="cursor-pointer text-2xl">&#9776;</section>
+      <section class="cursor-pointer text-2xl sm:hidden">&#9776;</section>
+      <section class="hidden cursor-pointer text-base font-semibold sm:block">
+        <ul class="flex space-x-6">
+          <li id="#rockets" class="hover:animate-bounce hover:text-slate-300">
+            Our Rockets
+          </li>
+          <li
+            id="#testimonials"
+            class="hover:animate-bounce hover:text-slate-300"
+          >
+            Testimonials
+          </li>
+          <li id="#contact" class="hover:animate-bounce hover:text-slate-300">
+            Contact Us
+          </li>
+        </ul>
+      </section>
     </section>
   </header>
 </template>
