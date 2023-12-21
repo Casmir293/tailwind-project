4c78a207c9f352213d39bd1941746d2e3b609586
Casmir Onyeka <casmir293@gmail.com>
2023-12-22 00:27:15 +0100

    added rocket component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <section class="font-mono">
+    <!-- header -->
     <header-comp />
+
+    <!-- our rockets -->
+    <rockets />
   </section>
 </template>
 
 <script setup>
 import HeaderComp from "./components/HeaderComp.vue";
+import Rockets from "./components/RocketsComp.vue";
 </script>
 
 <style scoped></style>
