1c9ad2b21735e1d2864f68a9fd687525fac8abf2
Casmir Onyeka <casmir293@gmail.com>
2023-12-22 00:31:54 +0100

    added hero component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <!-- header -->
     <header-comp />
 
+    <!-- hero -->
+    <hero />
+
     <!-- our rockets -->
     <rockets />
   </section>
@@ -10,6 +13,7 @@
 
 <script setup>
 import HeaderComp from "./components/HeaderComp.vue";
+import Hero from "./components/HeroComp.vue";
 import Rockets from "./components/RocketsComp.vue";
 </script>
 
