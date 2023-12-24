9b7e5e4b9ca80db05d58be07a1121fe8b7bf3e98
Casmir Onyeka <casmir293@gmail.com>
2023-12-24 17:56:07 +0100

    finished contact

--- src/components/ContactComp.vue
+++ src/components/ContactComp.vue
@@ -1,30 +1,30 @@
 <template>
-  <section id="contact">
-    <p class="mt-16 text-center text-2xl font-bold sm:mt-24 lg:text-3xl">
+  <section id="contact" class="mx-auto max-w-4xl px-3 pb-14 sm:px-5">
+    <p class="mb-5 mt-16 text-center text-2xl font-bold sm:mt-24 lg:text-3xl">
       Contact Us
     </p>
 
     <section>
       <form action="">
-        <label for="subject">Subject:</label><br />
+        <label for="subject" class="my-4 block">Subject:</label>
         <input
           type="text"
           id="subject"
           name="subject"
           placeholder="Your Subject"
-          class="rounded-xl p-3 text-black"
+          class="w-full rounded-xl px-3 py-4 text-black"
         />
         <br />
-        <label for="message">Message:</label> <br />
+        <label for="message" class="my-4 block">Message:</label>
         <textarea
           id="message"
           name="message"
+          rows="12"
           placeholder="Your Message"
-          class="rounded-xl p-3 text-black"
-        >
-        </textarea>
+          class="w-full rounded-xl px-3 py-4 text-black"
+        ></textarea>
         <br />
-        <button type="submit" class="mt-2 rounded-xl bg-green-800 px-8 py-2">
+        <button type="submit" class="mt-4 rounded-xl bg-green-800 px-12 py-3">
           Submit
         </button>
       </form>
